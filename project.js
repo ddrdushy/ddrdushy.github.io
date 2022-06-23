ef696de7fa9e77baff76ade90e6d50060722ad34
ddrdushy <dushy2009@gmail.com>
2022-06-23 22:51:29 +0800

    Project details added

---
+++ project.js
@@ -0,0 +1,158 @@
+var data = [
+  {
+    "title": "Outpatient registration process",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Health claims approval",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "MiCARE registration",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Letter Issuance",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Claim Extraction",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "GIRO account creation & Updation",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Petrol claims",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "MiCARE staff Claims",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Finance Receipting (5 Entities)",
+    "company": "Insurance Company",
+    "country": "Malaysia 2017",
+    "description": ""
+  },
+  {
+    "title": "Mobile API Process",
+    "company": "Insurance Company",
+    "country": "Malaysia 2018",
+    "description": ""
+  },
+  {
+    "title": "ESOA bot",
+    "company": "Insurance Company",
+    "country": "Malaysia 2018",
+    "description": ""
+  },
+  {
+    "title": "Solution design and project management",
+    "company": "Medical supply chain company",
+    "country": "Malaysia 2018",
+    "description": ""
+  },
+  {
+    "title": "UiPath training - 3 days",
+    "company": "Malaysian Airlines",
+    "country": "Malaysia 2020",
+    "description": ""
+  },
+  {
+    "title": "Solution design & project delivery",
+    "company": "Bank",
+    "country": "Malaysia 2020",
+    "description": ""
+  },
+  {
+    "title": "Inward claims process",
+    "company": "Insurance company in Malaysia",
+    "country": "Malaysia 2019",
+    "description": ""
+  },
+  {
+    "title": "Pre-sales",
+    "company": "IOI Malaysia",
+    "country": "Malaysia 2020",
+    "description": ""
+  },
+  {
+    "title": "Pre-sales & POC",
+    "company": "Swagelok Malaysia",
+    "country": "Malaysia 2019",
+    "description": ""
+  },
+  {
+    "title": "Pre-sales & Solution Design",
+    "company": "Hong Leong Bank Malaysia",
+    "country": "Malaysia 2019",
+    "description": ""
+  },
+  {
+    "title": "Pre-sales & POC",
+    "company": "The Pacific Insurance",
+    "country": "Malaysia 2019",
+    "description": ""
+  },
+  {
+    "title": "Pre-sales & POC",
+    "company": "The Pacific Insurance",
+    "country": "Malaysia 2019",
+    "description": ""
+  },
+  {
+    "title": "Training (Basic & Advance)",
+    "company": "Averis",
+    "country": "Malaysia 2021",
+    "description": ""
+  },
+  {
+    "title": "Solution Design",
+    "company": "Averis",
+    "country": "Malaysia 2021",
+    "description": ""
+  },
+  {
+    "title": "Process assessment & feasability check",
+    "company": "Bank Muamalat",
+    "country": "Malaysia 2022",
+    "description": ""
+  },
+  {
+    "title": "UiPath training",
+    "company": "JOS",
+    "country": "Malaysia 2021",
+    "description": ""
+  },
+  {
+    "title": "Solution Design & Development",
+    "company": "JPA",
+    "country": "Malaysia 2020",
+    "description": ""
+  },
+  {
+    "title": "Solution Design & Project delivery",
+    "company": "Seek Asia",
+    "country": "Malaysia 2022",
+    "description": ""
+  }
+];
